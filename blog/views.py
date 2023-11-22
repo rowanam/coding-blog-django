e23c319900b13ff66ae20e5a9899810b05cae6f7
rowanam <rowanamaas@gmail.com>
2023-11-22 23:06:34 +0000

    Display alert messages

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -49,6 +50,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.add_message(request, messages.SUCCESS, "Success! You left a comment.")
         else:
             comment_form = CommentForm()
 
