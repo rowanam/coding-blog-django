3550afe87dcf05ec944a2565de587db1e68fb770
rowanam <rowanamaas@gmail.com>
2023-11-22 00:48:38 +0000

    fix: add heroku host to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coding-blog-django-b6ba0cc97001.herokuapp.com']
 
 
 # Application definition
