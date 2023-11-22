48bb577b5f304f7df2845a689c450e91c3ed3554
rowanam <rowanamaas@gmail.com>
2023-11-22 23:17:14 +0000

    Deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG")
+DEBUG = False
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 ALLOWED_HOSTS = [
     "coding-blog-django-b6ba0cc97001.herokuapp.com",
